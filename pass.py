71ae3c45198d90efa0697a9c4405cfa1956b88ce
Mikołaj Lubiak <mikolajlubiak@no-reply@disroot.org>
2022-06-17 16:42:19 +0000

    Refactorization

--- pass.py
+++ pass.py
@@ -7,16 +7,11 @@ def password(n=256):
     chars = tuple(printable)
     randints = []
     passwd = ""
-
     for i in range(n):
         randints.append(randint(0, int(time())%(len(chars)+1)))
-
     for i in randints:
         passwd += chars[i]
-
     return passwd
 
-def main():
+if __name__ == "__main__":
     print(password(int(argv[1])))
-
-main()
