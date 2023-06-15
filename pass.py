88fb4a2bfad561ecbd69aa59c503e34bbf2e2c2b
Mikołaj Lubiak <lubiak@proton.me>
2023-06-15 17:11:32 +0200

    Much more improved version

--- pass.py
+++ pass.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python
 
 from sys import argv
-from time import time
-from random import randint
-from string import printable
+from string import punctuation, digits, ascii_letters
+import secrets
+
+chars = punctuation + digits + ascii_letters
 
 def password(n=256):
-    chars = tuple(printable)
-    randints = []
-    passwd = ""
-    for i in range(n):
-        randints.append(randint(0, int(time())%(len(chars)+1)))
-    for i in randints:
-        passwd += chars[i]
-    return passwd
+    return ''.join(secrets.choice(chars) for i in range(n))
 
 if __name__ == "__main__":
-    print(password(int(argv[1])))
+    try:
+        length = int(argv[1])
+    except:
+        length = int(input("Password length: "))
+    print(password(length))
